daa37bec479d3eb38d5ea90099ea905c83d4c757
dev <hwonderoftheworld@gmail.com>
2022-10-29 18:29:30 -0700

    single footer item

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,33 +65,8 @@ export const Footer = () => {
     <Container>
       <LanugageDropdown invert />
       <TrailingStack>
-        <LinkContainer>
-          <StyledNavLink disabled href="/bug-bounty">
-            {t('navigation.bugBounty')}
-          </StyledNavLink>
-          <StyledNavLink disabled href="/media-kit">
-            {t('navigation.mediaKit')}
-          </StyledNavLink>
-        </LinkContainer>
         <SocialIconContainer>
-          <SocialIcon Icon={SocialTwitter} color="#5298FF" href="https://twitter.com/ensdomains" />
-          <SocialIcon Icon={SocialGithub} color="#0F0F0F" href="https://github.com/ensdomains" />
-          <SocialIcon Icon={SocialDiscord} color="#7F83FF" href="https://chat.ens.domains" />
-          <SocialIcon
-            Icon={SocialMedium}
-            color="#0F0F0F"
-            href="https://medium.com/the-ethereum-name-service"
-          />
-          <SocialIcon
-            Icon={SocialDiscourse}
-            ColoredIcon={SocialDiscourseColour}
-            href="https://discuss.ens.domains/"
-          />
-          <SocialIcon
-            Icon={SocialYoutube}
-            color="#EE1919"
-            href="https://www.youtube.com/ensdomains"
-          />
+          <SocialIcon Icon={SocialTwitter} color="#5298FF" href="https://twitter.com/bootypoppin" />
         </SocialIconContainer>
       </TrailingStack>
     </Container>
